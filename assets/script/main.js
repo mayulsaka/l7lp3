722fc905ecd1e766dc3b6b35b2196884f92397dd
mayulsaka <mayumilhotesakaguchi@yahoo.co.jp>
2025-01-02 13:12:41 +0900

    250102v2(提出)

--- assets/script/main.js
+++ assets/script/main.js
@@ -4,9 +4,9 @@
 $('.visual').slick({
   dots: true,
   slidesToShow: 3,
-  slidesToScroll:3,
-  autoplay: false,
-  autoplaySpeed: 5500,
+  slidesToScroll:2,
+  autoplay: true, 
+  autoplaySpeed: 5000,
   responsive: [
     {
       breakpoint: 767.9, 
